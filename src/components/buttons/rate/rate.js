cf6b2f06a2795f0a42c4002fba47f8a07aad3cc8
FrusE1 <fathutdinov.2000@mail.ru>
2021-12-02 17:05:29 +0300

    changed component rate

---
+++ src/components/buttons/rate/rate.js
@@ -0,0 +1,8 @@
+const rateItemsList = document.querySelectorAll('.rate__item');
+const rateItemsArray = Array.prototype.slice.call(rateItemsList);
+
+rateItemsArray.forEach(item => {
+	item.addEventListener("click", () => {
+		item.parentNode.dataset.totalRate = item.dataset.itemRate;
+	})
+})
