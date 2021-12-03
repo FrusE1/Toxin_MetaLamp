264979fbe70169c562e321e359a5bfaa00c67da4
FrusE1 <fathutdinov.2000@mail.ru>
2021-12-03 17:41:19 +0300

    completed component slider and minor edits

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -5,7 +5,7 @@ const rangeSlider = document.getElementById('slider-block');
 
 if (rangeSlider) {
 	noUiSlider.create(rangeSlider, {
-		start: [1000, 15000],
+		start: [5000, 10000],
 		connect: true,
 		step: 1,
 		range: {
@@ -14,3 +14,22 @@ if (rangeSlider) {
 		}
 	});
 }
+
+const input0 = document.getElementById("input-0");
+const input1 = document.getElementById("input-1");
+const inputs = [input0, input1];
+
+rangeSlider.noUiSlider.on('update', function (values, handle) {
+	inputs[handle].value = Math.round(values[handle]);
+})
+
+const setRangeSlider = (i, value) => {
+	let arr = [null, null];
+	arr[i] = value;
+	rangeSlider.noUiSlider.set(arr)
+}
+inputs.forEach((elem, index) => {
+	elem.addEventListener('change', (e) => {
+		setRangeSlider(index, e.currentTarget.value)
+	})
+})
