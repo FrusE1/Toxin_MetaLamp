204b5da8ac3e2cef110d2c260fe2b27ff22b3dc1
FrusE1 <fathutdinov.2000@mail.ru>
2021-12-17 08:36:46 +0300

    added button apply

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,41 +1,15 @@
-// const counter = function () {
-// 	const btns = document.querySelectorAll('.dropdown-guests__btn');
-// 	btns.forEach(btn => {
-// 		btn.addEventListener('click', function (event) {
-// 			const direction = this.dataset.direction;
-// 			const inp = this.parentElement.querySelector('.dropdown-guests__value');
-// 			const currentValue = +inp.textContent;
-// 			let newValue;
-// 			if (direction === 'plus') {
-// 				newValue = currentValue + 1;
-// 			} else {
-// 				newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
-// 			}
-// 			inp.textContent = newValue;
-// 			const inputDrop = document.querySelector('.dropdown-guests__input');
-// 			if (newValue <= 1) {
-// 				inputDrop.value = `${newValue} гость`;
-// 			} else if (newValue <= 4) {
-// 				inputDrop.value = `${newValue} гостя`;
-// 			} else if (newValue > 4) {
-// 				inputDrop.value = `${newValue} гостей`;
-// 			}
-// 			event.preventDefault();
-// 		});
-// 	})
-// }
-// counter();
+// guest counter
 
-const inputDrop = document.querySelectorAll('.dropdown-guests__value');
+const valueDropGuest = document.querySelectorAll('.dropdown-guests__value');
 
-const btnMinusOne = inputDrop[0].previousElementSibling;
-const btnPlusOne = inputDrop[0].nextElementSibling;
-let inputVarOne = +inputDrop[0].textContent;
-const inputDropTest = document.querySelector('.dropdown-guests__input');
-const buttonClear = document.querySelector('.dropdown-guests-btn__clear');
+const btnMinusOne = valueDropGuest[0].previousElementSibling;
+const btnPlusOne = valueDropGuest[0].nextElementSibling;
+let inputVarOne = +valueDropGuest[0].textContent;
+const inputDropGuest = document.querySelector('.dropdown-guests__input');
+const btnDropClear = document.querySelector('.dropdown-guests-btn__clear');
 
 btnPlusOne.addEventListener("click", function (event) {
-	inputDrop[0].textContent = ++inputVarOne;
+	valueDropGuest[0].textContent = ++inputVarOne;
 	let babyVar;
 	if (inputVarThree == 0) {
 		babyVar = ``;
@@ -48,19 +22,20 @@ btnPlusOne.addEventListener("click", function (event) {
 	}
 	let inputVarSumm = inputVarOne + inputVarTwo + inputVarThree;
 	if (inputVarSumm == 0) {
-		inputDropTest.value = "Сколько гостей";
+		inputDropGuest.value = "Сколько гостей";
 	} else if (inputVarSumm == 1) {
-		inputDropTest.value = `${inputVarSumm} гость${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гость${babyVar}`;
 	} else if (inputVarSumm <= 4) {
-		inputDropTest.value = `${inputVarSumm} гостя${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостя${babyVar}`;
 	} else if (inputVarSumm > 4) {
-		inputDropTest.value = `${inputVarSumm} гостей${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостей${babyVar}`;
 	}
 	event.preventDefault();
 });
+
 btnMinusOne.addEventListener("click", function (event) {
 	inputVarOne = inputVarOne > 0 ? --inputVarOne : 0
-	inputDrop[0].textContent = inputVarOne;
+	valueDropGuest[0].textContent = inputVarOne;
 	let babyVar;
 	if (inputVarThree == 0) {
 		babyVar = ``;
@@ -73,23 +48,24 @@ btnMinusOne.addEventListener("click", function (event) {
 	}
 	let inputVarSumm = inputVarOne + inputVarTwo + inputVarThree;
 	if (inputVarSumm == 0) {
-		inputDropTest.value = "Сколько гостей";
+		inputDropGuest.value = "Сколько гостей";
 	} else if (inputVarSumm == 1) {
-		inputDropTest.value = `${inputVarSumm} гость${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гость${babyVar}`;
 	} else if (inputVarSumm <= 4) {
-		inputDropTest.value = `${inputVarSumm} гостя${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостя${babyVar}`;
 	} else if (inputVarSumm > 4) {
-		inputDropTest.value = `${inputVarSumm} гостей${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостей${babyVar}`;
 	}
 	event.preventDefault();
 })
 
 
-const btnMinusTwo = inputDrop[1].previousElementSibling;
-const btnPlusTwo = inputDrop[1].nextElementSibling;
-let inputVarTwo = +inputDrop[1].textContent;
+const btnMinusTwo = valueDropGuest[1].previousElementSibling;
+const btnPlusTwo = valueDropGuest[1].nextElementSibling;
+let inputVarTwo = +valueDropGuest[1].textContent;
+
 btnPlusTwo.addEventListener("click", function (event) {
-	inputDrop[1].textContent = ++inputVarTwo;
+	valueDropGuest[1].textContent = ++inputVarTwo;
 	let babyVar;
 	if (inputVarThree == 0) {
 		babyVar = ``;
@@ -102,19 +78,20 @@ btnPlusTwo.addEventListener("click", function (event) {
 	}
 	let inputVarSumm = inputVarOne + inputVarTwo + inputVarThree;
 	if (inputVarSumm == 0) {
-		inputDropTest.value = "Сколько гостей";
+		inputDropGuest.value = "Сколько гостей";
 	} else if (inputVarSumm == 1) {
-		inputDropTest.value = `${inputVarSumm} гость${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гость${babyVar}`;
 	} else if (inputVarSumm <= 4) {
-		inputDropTest.value = `${inputVarSumm} гостя${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостя${babyVar}`;
 	} else if (inputVarSumm > 4) {
-		inputDropTest.value = `${inputVarSumm} гостей${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостей${babyVar}`;
 	}
 	event.preventDefault();
 });
+
 btnMinusTwo.addEventListener("click", function (event) {
 	inputVarTwo = inputVarTwo > 0 ? --inputVarTwo : 0;
-	inputDrop[1].textContent = inputVarTwo;
+	valueDropGuest[1].textContent = inputVarTwo;
 	let babyVar;
 	if (inputVarThree == 0) {
 		babyVar = ``;
@@ -127,22 +104,23 @@ btnMinusTwo.addEventListener("click", function (event) {
 	}
 	let inputVarSumm = inputVarOne + inputVarTwo + inputVarThree;
 	if (inputVarSumm == 0) {
-		inputDropTest.value = "Сколько гостей";
+		inputDropGuest.value = "Сколько гостей";
 	} else if (inputVarSumm == 1) {
-		inputDropTest.value = `${inputVarSumm} гость${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гость${babyVar}`;
 	} else if (inputVarSumm <= 4) {
-		inputDropTest.value = `${inputVarSumm} гостя${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостя${babyVar}`;
 	} else if (inputVarSumm > 4) {
-		inputDropTest.value = `${inputVarSumm} гостей${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостей${babyVar}`;
 	}
 	event.preventDefault();
 })
 
-const btnMinusThree = inputDrop[2].previousElementSibling;
-const btnPlusThree = inputDrop[2].nextElementSibling;
-let inputVarThree = +inputDrop[2].textContent;
+const btnMinusThree = valueDropGuest[2].previousElementSibling;
+const btnPlusThree = valueDropGuest[2].nextElementSibling;
+let inputVarThree = +valueDropGuest[2].textContent;
+
 btnPlusThree.addEventListener("click", function (event) {
-	inputDrop[2].textContent = ++inputVarThree;
+	valueDropGuest[2].textContent = ++inputVarThree;
 	let babyVar;
 	if (inputVarThree == 0) {
 		babyVar = ``;
@@ -155,19 +133,20 @@ btnPlusThree.addEventListener("click", function (event) {
 	}
 	let inputVarSumm = inputVarOne + inputVarTwo + inputVarThree;
 	if (inputVarSumm == 0) {
-		inputDropTest.value = "Сколько гостей";
+		inputDropGuest.value = "Сколько гостей";
 	} else if (inputVarSumm == 1) {
-		inputDropTest.value = `${inputVarSumm} гость${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гость${babyVar}`;
 	} else if (inputVarSumm <= 4) {
-		inputDropTest.value = `${inputVarSumm} гостя${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостя${babyVar}`;
 	} else if (inputVarSumm > 4) {
-		inputDropTest.value = `${inputVarSumm} гостей${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостей${babyVar}`;
 	}
 	event.preventDefault();
 });
+
 btnMinusThree.addEventListener("click", function (event) {
 	inputVarThree = inputVarThree > 0 ? --inputVarThree : 0;
-	inputDrop[2].textContent = inputVarThree;
+	valueDropGuest[2].textContent = inputVarThree;
 	let babyVar;
 	if (inputVarThree == 0) {
 		babyVar = ``;
@@ -180,24 +159,26 @@ btnMinusThree.addEventListener("click", function (event) {
 	}
 	let inputVarSumm = inputVarOne + inputVarTwo + inputVarThree;
 	if (inputVarSumm == 0) {
-		inputDropTest.value = "Сколько гостей";
+		inputDropGuest.value = "Сколько гостей";
 	} else if (inputVarSumm == 1) {
-		inputDropTest.value = `${inputVarSumm} гость${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гость${babyVar}`;
 	} else if (inputVarSumm <= 4) {
-		inputDropTest.value = `${inputVarSumm} гостя${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостя${babyVar}`;
 	} else if (inputVarSumm > 4) {
-		inputDropTest.value = `${inputVarSumm} гостей${babyVar}`;
+		inputDropGuest.value = `${inputVarSumm} гостей${babyVar}`;
 	}
 	event.preventDefault();
 })
 
-buttonClear.addEventListener("click", function (event) {
-	inputDropTest.value = 'Сколько гостей';
-	inputDrop[0].textContent = "0";
+// clear button
+
+btnDropClear.addEventListener("click", function (event) {
+	inputDropGuest.value = 'Сколько гостей';
+	valueDropGuest[0].textContent = "0";
 	inputVarOne = 0;
-	inputDrop[1].textContent = "0";
+	valueDropGuest[1].textContent = "0";
 	inputVarTwo = 0;
-	inputDrop[2].textContent = "0";
+	valueDropGuest[2].textContent = "0";
 	inputVarThree = 0;
 	event.preventDefault();
 })
@@ -205,6 +186,15 @@ buttonClear.addEventListener("click", function (event) {
 // expading the list
 
 const listDrop = document.querySelector('.dropdown-guests');
+
+const btnDropReady = document.querySelector('.dropdown-guests-btn__ready');
+
+
+
+btnDropReady.addEventListener("click", function (event) { // apply button
+	listDrop.classList.remove('_active-dropdown');
+	event.preventDefault();
+})
 
 document.addEventListener("click", list);
 
