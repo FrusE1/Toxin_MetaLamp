819cf97885540aa9648e3730e6d8f7dbf375a541
FrusE1 <fathutdinov.2000@mail.ru>
2021-12-21 13:40:12 +0300

    pixel perfect form-element block

--- src/components/buttons/paginator/paginator.js
+++ src/components/buttons/paginator/paginator.js
@@ -1,50 +1,50 @@
-const ulTag = document.querySelector('.paginator__list');
+// const ulTag = document.querySelector('.paginator__list');
 
-let totalPages = 15;
-function element(totalPages, page) {
-	let liTag = '';
-	let activeLi;
-	let beforePages = page - 1;
-	let afterPages = page + 1;
-	if (page > 1) {
-		liTag += `<li class="paginator__li-prev"><span class="paginator__span"><i class="paginator__arrow paginator__arrow-prev icon-arrow"></i></span></li>`;
-	}
+// let totalPages = 15;
+// function element(totalPages, page) {
+// 	let liTag = '';
+// 	let activeLi;
+// 	let beforePages = page - 1;
+// 	let afterPages = page + 1;
+// 	if (page > 1) {
+// 		liTag += `<li class="paginator__li-prev"><span class="paginator__span"><i class="paginator__arrow paginator__arrow-prev icon-arrow"></i></span></li>`;
+// 	}
 
-	if (page > 2) {
-		liTag += `<li class="paginator__number" onclick="element(totalPages, 1)"><span>1</span></li>`;
-		if (page > 3) {
-			liTag += `<li class="paginator__dots"><span>...</span></li>`;
-		}
-	}
+// 	if (page > 2) {
+// 		liTag += `<li class="paginator__number" onclick="element(totalPages, 1)"><span>1</span></li>`;
+// 		if (page > 3) {
+// 			liTag += `<li class="paginator__dots"><span>...</span></li>`;
+// 		}
+// 	}
 
-	for (let pageLenght = beforePages; pageLenght <= afterPages; pageLenght++) {
-		if (pageLenght > totalPages) {
-			continue;
-		}
+// 	for (let pageLenght = beforePages; pageLenght <= afterPages; pageLenght++) {
+// 		if (pageLenght > totalPages) {
+// 			continue;
+// 		}
 
-		if (pageLenght == 0) {
-			pageLenght = pageLenght + 1;
-		}
+// 		if (pageLenght == 0) {
+// 			pageLenght = pageLenght + 1;
+// 		}
 
-		if (page == pageLenght) {
-			activeLi = "paginator__number_active";
-		} else {
-			activeLi = "";
-		}
+// 		if (page == pageLenght) {
+// 			activeLi = "paginator__number_active";
+// 		} else {
+// 			activeLi = "";
+// 		}
 
-		liTag += `<li class="paginator__number ${activeLi}" onclick="element(totalPages, ${pageLenght})"><span>${pageLenght}</span></li>`;
-	}
+// 		liTag += `<li class="paginator__number ${activeLi}" onclick="element(totalPages, ${pageLenght})"><span>${pageLenght}</span></li>`;
+// 	}
 
-	if (page < totalPages - 1) {
-		if (page < totalPages - 2) {
-			liTag += `<li class="paginator__dots"><span>...</span></li>`;
-		}
-		liTag += `<li class="paginator__number" onclick="element(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
-	}
+// 	if (page < totalPages - 1) {
+// 		if (page < totalPages - 2) {
+// 			liTag += `<li class="paginator__dots"><span>...</span></li>`;
+// 		}
+// 		liTag += `<li class="paginator__number" onclick="element(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
+// 	}
 
-	if (page < totalPages) {
-		liTag += `<li class="paginator__li-next" onclick="element(totalPages, ${page + 1})"><span class="paginator__span"><i class="paginator__arrow paginator__arrow-next icon-arrow"></i></span></li>`
-	}
-	ulTag.innerHTML = liTag;
-}
-element(totalPages, 1);
+// 	if (page < totalPages) {
+// 		liTag += `<li class="paginator__li-next" onclick="element(totalPages, ${page + 1})"><span class="paginator__span"><i class="paginator__arrow paginator__arrow-next icon-arrow"></i></span></li>`
+// 	}
+// 	ulTag.innerHTML = liTag;
+// }
+// element(totalPages, 1);
