26aec1384dfb992890fe12e5af9db2199904471f
FrusE1 <fathutdinov.2000@mail.ru>
2021-12-27 09:59:22 +0300

    improved file dropdown.js

--- src/components/dropdown/dropdown-guests/dropdown-guests.js
+++ src/components/dropdown/dropdown-guests/dropdown-guests.js
@@ -1,129 +1,126 @@
 if (document.querySelector('.dropdown-guests')) { // проверка наличия dropdown-guests
 	// --- Подсчет количества гостей
-	const dropGuests = document.querySelector('.dropdown-guests__block');
-	const inputDropGuests = document.querySelector('.dropdown-guests__input');
-	const valueDropGuests = document.querySelectorAll('.dropdown-guests__value');
-	const btnDropMinus = document.querySelectorAll('.dropdown-guests__btn_minus')
-	let inputDropVar = inputDropGuests.value;
+	const dropGuestsBlock = document.querySelectorAll('.dropdown-guests');
+	dropGuestsBlock.forEach(dropGuestsElem => {
+		dropGuestsElem.addEventListener("click", function (event) {
+			const inputDropGuests = dropGuestsElem.querySelector('.dropdown-guests__input');
+			const valueDropGuests = dropGuestsElem.querySelectorAll('.dropdown-guests__value');
+			const btnDropMinus = dropGuestsElem.querySelectorAll('.dropdown-guests__btn_minus');
+			let inputGuestsVar = "Сколько гостей";
 
-	// Значения установленные заранее
-	let valueDropOnePrev = +valueDropGuests[0].textContent;
-	let valueDropTwoPrev = +valueDropGuests[1].textContent;
-	let valueDropThreePrev = +valueDropGuests[2].textContent;
-	let valueDropSummPrev = valueDropOnePrev + valueDropTwoPrev + valueDropThreePrev;
-	let babyDropPrev;
-	if (valueDropThreePrev == 0) {
-		babyDropPrev = ``;
-	} else if (valueDropThreePrev == 1) {
-		babyDropPrev = `, ${valueDropThreePrev} младенец`;
-	}
-	else if (valueDropThreePrev <= 4) {
-		babyDropPrev = `, ${valueDropThreePrev} младенца`;
-	}
-	else if (valueDropThreePrev > 4) {
-		babyDropPrev = `, ${valueDropThreePrev} младенцев`;
-	};
-	if (valueDropSummPrev == 0) {
-		inputDropGuests.value = inputDropVar;
-	} else if (valueDropSummPrev == 1) {
-		inputDropGuests.value = `${valueDropSummPrev} гость${babyDropPrev}`;
-	} else if (valueDropSummPrev <= 4) {
-		inputDropGuests.value = `${valueDropSummPrev} гостя${babyDropPrev}`;
-	} else if (valueDropSummPrev > 4) {
-		inputDropGuests.value = `${valueDropSummPrev} гостей${babyDropPrev}`;
-	};
-
-	if (valueDropOnePrev >= 1) {
-		btnDropMinus[0].style.color = "rgba(31, 32, 65, 0.5)";
-		btnDropMinus[0].style.borderColor = "rgba(31, 32, 65, 0.5)";
-	}
-	if (valueDropTwoPrev >= 1) {
-		btnDropMinus[1].style.color = "rgba(31, 32, 65, 0.5)";
-		btnDropMinus[1].style.borderColor = "rgba(31, 32, 65, 0.5)";
-	}
-	if (valueDropThreePrev >= 1) {
-		btnDropMinus[2].style.color = "rgba(31, 32, 65, 0.5)";
-		btnDropMinus[2].style.borderColor = "rgba(31, 32, 65, 0.5)";
-	}
-
-	// Основной код подсчета гостей
-	dropGuests.addEventListener("click", function (event) {
-		let targetDrop = event.target;
-		let dropSpan;
-		if (targetDrop.dataset.direction == "plus") {
-			dropSpan = +targetDrop.previousElementSibling.textContent;
-			targetDrop.previousElementSibling.textContent = ++dropSpan;
-			if (dropSpan >= 1) {
-				targetDrop.parentNode.firstElementChild.style.color = "rgba(31, 32, 65, 0.5)";
-				targetDrop.parentNode.firstElementChild.style.borderColor = "rgba(31, 32, 65, 0.5)";
+			// Значения установленные заранее
+			let valueDropOnePrev = +valueDropGuests[0].textContent;
+			let valueDropTwoPrev = +valueDropGuests[1].textContent;
+			let valueDropThreePrev = +valueDropGuests[2].textContent;
+			let valueDropSummPrev = valueDropOnePrev + valueDropTwoPrev + valueDropThreePrev;
+			let babyDropPrev;
+			if (valueDropThreePrev == 0) {
+				babyDropPrev = ``;
+			} else if (valueDropThreePrev == 1) {
+				babyDropPrev = `, ${valueDropThreePrev} младенец`;
 			}
-
-		}
-		if (targetDrop.dataset.direction == "minus") {
-			dropSpan = +targetDrop.nextElementSibling.textContent;
-			dropSpan = dropSpan > 0 ? --dropSpan : 0;
-			if (dropSpan == 0) {
-				targetDrop.style.color = "rgba(31, 32, 65, 0.25)";
-				targetDrop.style.borderColor = "rgba(31, 32, 65, 0.25)";
+			else if (valueDropThreePrev <= 4) {
+				babyDropPrev = `, ${valueDropThreePrev} младенца`;
 			}
-			targetDrop.nextElementSibling.textContent = dropSpan;
-		}
-		let valueDropOne = +valueDropGuests[0].textContent;
-		let valueDropTwo = +valueDropGuests[1].textContent;
-		let valueDropThree = +valueDropGuests[2].textContent;
-		let valueSumm = valueDropOne + valueDropTwo + valueDropThree;
-		let babyDrop;
-		if (valueDropThree == 0) {
-			babyDrop = ``;
-		} else if (valueDropThree == 0) {
-			babyDrop = `, ${valueDropThree} младенец`;
-		}
-		else if (valueDropThree <= 4) {
-			babyDrop = `, ${valueDropThree} младенца`;
-		}
-		else if (valueDropThree > 4) {
-			babyDrop = `, ${valueDropThree} младенцев`;
-		};
-		if (valueSumm == 0) {
-			inputDropGuests.value = inputDropVar;
-		} else if (valueSumm == 1) {
-			inputDropGuests.value = `${valueSumm} гость${babyDrop}`;
-		} else if (valueSumm <= 4) {
-			inputDropGuests.value = `${valueSumm} гостя${babyDrop}`;
-		} else if (valueSumm > 4) {
-			inputDropGuests.value = `${valueSumm} гостей${babyDrop}`;
-		};
-		if (targetDrop.dataset.clear == 'clear') {
-			valueDropGuests[0].textContent = 0;
-			valueDropGuests[1].textContent = 0;
-			valueDropGuests[2].textContent = 0;
-			inputDropGuests.value = inputDropVar;
-			let clearBtn = targetDrop.parentNode.previousElementSibling.querySelectorAll('.dropdown-guests__btn_minus');
-			clearBtn.forEach(clearBtnElem => {
-				clearBtnElem.style.borderColor = "rgba(31, 32, 65, 0.25)";
-				clearBtnElem.style.color = "rgba(31, 32, 65, 0.25)";
+			else if (valueDropThreePrev > 4) {
+				babyDropPrev = `, ${valueDropThreePrev} младенцев`;
+			};
+			if (valueDropSummPrev == 0) {
+				inputDropGuests.value = inputGuestsVar;
+			} else if (valueDropSummPrev == 1) {
+				inputDropGuests.value = `${valueDropSummPrev} гость${babyDropPrev}`;
+			} else if (valueDropSummPrev <= 4) {
+				inputDropGuests.value = `${valueDropSummPrev} гостя${babyDropPrev}`;
+			} else if (valueDropSummPrev > 4) {
+				inputDropGuests.value = `${valueDropSummPrev} гостей${babyDropPrev}`;
+			};
+
+			if (valueDropOnePrev >= 1) {
+				btnDropMinus[0].style.color = "rgba(31, 32, 65, 0.5)";
+				btnDropMinus[0].style.borderColor = "rgba(31, 32, 65, 0.5)";
+			}
+			if (valueDropTwoPrev >= 1) {
+				btnDropMinus[1].style.color = "rgba(31, 32, 65, 0.5)";
+				btnDropMinus[1].style.borderColor = "rgba(31, 32, 65, 0.5)";
+			}
+			if (valueDropThreePrev >= 1) {
+				btnDropMinus[2].style.color = "rgba(31, 32, 65, 0.5)";
+				btnDropMinus[2].style.borderColor = "rgba(31, 32, 65, 0.5)";
+			}
+			// Основной код подсчета гостей
+			let targetDrop = event.target;
+			let dropSpan;
+			if (targetDrop.dataset.direction == "plus") {
+				dropSpan = +targetDrop.previousElementSibling.textContent;
+				targetDrop.previousElementSibling.textContent = ++dropSpan;
+				if (dropSpan >= 1) {
+					targetDrop.parentNode.firstElementChild.style.color = "rgba(31, 32, 65, 0.5)";
+					targetDrop.parentNode.firstElementChild.style.borderColor = "rgba(31, 32, 65, 0.5)";
+				};
+			};
+			if (targetDrop.dataset.direction == "minus") {
+				dropSpan = +targetDrop.nextElementSibling.textContent;
+				dropSpan = dropSpan > 0 ? --dropSpan : 0;
+				if (dropSpan == 0) {
+					targetDrop.style.color = "rgba(31, 32, 65, 0.25)";
+					targetDrop.style.borderColor = "rgba(31, 32, 65, 0.25)";
+				}
+				targetDrop.nextElementSibling.textContent = dropSpan;
+			}
+			let valueDropOne = +valueDropGuests[0].textContent;
+			let valueDropTwo = +valueDropGuests[1].textContent;
+			let valueDropThree = +valueDropGuests[2].textContent;
+			let valueSumm = valueDropOne + valueDropTwo + valueDropThree;
+			let babyDrop;
+			if (valueDropThree == 0) {
+				babyDrop = ``;
+			} else if (valueDropThree == 0) {
+				babyDrop = `, ${valueDropThree} младенец`;
+			}
+			else if (valueDropThree <= 4) {
+				babyDrop = `, ${valueDropThree} младенца`;
+			}
+			else if (valueDropThree > 4) {
+				babyDrop = `, ${valueDropThree} младенцев`;
+			};
+			if (valueSumm == 0) {
+				inputDropGuests.value = inputGuestsVar;
+			} else if (valueSumm == 1) {
+				inputDropGuests.value = `${valueSumm} гость${babyDrop}`;
+			} else if (valueSumm <= 4) {
+				inputDropGuests.value = `${valueSumm} гостя${babyDrop}`;
+			} else if (valueSumm > 4) {
+				inputDropGuests.value = `${valueSumm} гостей${babyDrop}`;
+			};
+			if (targetDrop.dataset.clear == 'clear') { // Кнопка "очистить"
+				valueDropGuests[0].textContent = 0;
+				valueDropGuests[1].textContent = 0;
+				valueDropGuests[2].textContent = 0;
+				inputDropGuests.value = inputGuestsVar;
+				let clearBtn = targetDrop.parentNode.previousElementSibling.querySelectorAll('.dropdown-guests__btn_minus');
+				clearBtn.forEach(clearBtnElem => {
+					clearBtnElem.style.borderColor = "rgba(31, 32, 65, 0.25)";
+					clearBtnElem.style.color = "rgba(31, 32, 65, 0.25)";
+				});
+			};
+			// --- Выпадаюший список гостей
+			const btnDropReady = dropGuestsElem.querySelector('.dropdown-guests-btn__ready');
+
+			btnDropReady.addEventListener("click", function (event) { // Кнопка "применить"
+				dropGuestsElem.classList.remove('_active-dropdown-guests');
+				event.preventDefault();
 			});
-		};
-		event.preventDefault();
-	})
 
-	// --- Выпадаюший список гостей
-	const listDrop = document.querySelector('.dropdown-guests');
-	const btnDropReady = document.querySelector('.dropdown-guests-btn__ready');
-
-	btnDropReady.addEventListener("click", function (event) { // Кнопка "применить"
-		listDrop.classList.remove('_active-dropdown-guests');
-		event.preventDefault();
+			document.addEventListener("click", showList);
+			function showList(event) {
+				if (event.target.closest('.dropdown-guests__input')) {
+					event.target.parentNode.classList.toggle('_active-dropdown-guests');
+				};
+				if (!event.target.closest('.dropdown-guests')) {
+					dropGuestsElem.classList.remove('_active-dropdown-guests');
+				};
+			};
+			event.preventDefault();
+		});
 	});
-
-	document.addEventListener("click", list);
-
-	function list(event) {
-		if (event.target.closest('.dropdown-guests__input')) {
-			listDrop.classList.toggle('_active-dropdown-guests');
-		};
-		if (!event.target.closest('.dropdown-guests')) {
-			listDrop.classList.remove('_active-dropdown-guests');
-		};
-	};
 };
--- src/components/dropdown/dropdown-room/dropdown-room.js
+++ src/components/dropdown/dropdown-room/dropdown-room.js
@@ -1,168 +1,168 @@
 if (document.querySelector('.dropdown-room__block')) { // проверка наличия dropdown-room
-	// --- Подсчет количества комнат
-	const dropRoom = document.querySelector('.dropdown-room__block');
-	const inputDropRoom = document.querySelector('.dropdown-room__input');
-	const valueDropRoom = document.querySelectorAll('.dropdown-room__value');
-	const btnDropRoomMinus = document.querySelectorAll('.dropdown-room__btn_minus')
-	let inputDropRoomVar = inputDropRoom.value;
-	// Значения установленные заранее
-	let valueDropRoomOnePrev = +valueDropRoom[0].textContent;
-	let valueDropRoomTwoPrev = +valueDropRoom[1].textContent;
-	let valueDropRoomThreePrev = +valueDropRoom[2].textContent;
+	const dropRoomBlock = document.querySelectorAll('.dropdown-room');
+	dropRoomBlock.forEach(dropRoomElem => {
+		dropRoomElem.addEventListener("click", function (event) {
+			// --- Подсчет количества комнат
+			const inputDropRoom = dropRoomElem.querySelector('.dropdown-room__input');
+			const valueDropRoom = dropRoomElem.querySelectorAll('.dropdown-room__value');
+			const btnDropRoomMinus = dropRoomElem.querySelectorAll('.dropdown-room__btn_minus')
+			let inputDropRoomVar = "Сколько комнат";
+			// Значения установленные заранее
+			let valueDropRoomOnePrev = +valueDropRoom[0].textContent;
+			let valueDropRoomTwoPrev = +valueDropRoom[1].textContent;
+			let valueDropRoomThreePrev = +valueDropRoom[2].textContent;
 
-	let bedroomsPrev;
-	if (valueDropRoomOnePrev == 0) {
-		bedroomsPrev = `0 спален`;
-	} else if (valueDropRoomOnePrev == 1) {
-		bedroomsPrev = `${valueDropRoomOnePrev} спальня`;
-	}
-	else if (valueDropRoomOnePrev <= 4) {
-		bedroomsPrev = `${valueDropRoomOnePrev} спальни`;
-	}
-	else if (valueDropRoomOnePrev > 4) {
-		bedroomsPrev = `${valueDropRoomOnePrev} спален`;
-	};
+			let bedroomsPrev;
+			if (valueDropRoomOnePrev == 0) {
+				bedroomsPrev = `0 спален`;
+			} else if (valueDropRoomOnePrev == 1) {
+				bedroomsPrev = `${valueDropRoomOnePrev} спальня`;
+			}
+			else if (valueDropRoomOnePrev <= 4) {
+				bedroomsPrev = `${valueDropRoomOnePrev} спальни`;
+			}
+			else if (valueDropRoomOnePrev > 4) {
+				bedroomsPrev = `${valueDropRoomOnePrev} спален`;
+			};
 
-	let bedDropPrev;
-	if (valueDropRoomTwoPrev == 0) {
-		bedDropPrev = ``;
-	} else if (valueDropRoomTwoPrev == 1) {
-		bedDropPrev = `, ${valueDropRoomTwoPrev} кровать`;
-	}
-	else if (valueDropRoomTwoPrev <= 4) {
-		bedDropPrev = `, ${valueDropRoomTwoPrev} кровати`;
-	}
-	else if (valueDropRoomTwoPrev > 4) {
-		bedDropPrev = `, ${valueDropRoomTwoPrev} кроватей`;
-	};
+			let bedDropPrev;
+			if (valueDropRoomTwoPrev == 0) {
+				bedDropPrev = ``;
+			} else if (valueDropRoomTwoPrev == 1) {
+				bedDropPrev = `, ${valueDropRoomTwoPrev} кровать`;
+			}
+			else if (valueDropRoomTwoPrev <= 4) {
+				bedDropPrev = `, ${valueDropRoomTwoPrev} кровати`;
+			}
+			else if (valueDropRoomTwoPrev > 4) {
+				bedDropPrev = `, ${valueDropRoomTwoPrev} кроватей`;
+			};
 
-	let bathroomsDropPrev;
-	if (valueDropRoomThreePrev == 0) {
-		bathroomsDropPrev = ``;
-	} else if (valueDropRoomThreePrev == 1) {
-		bathroomsDropPrev = `, ${valueDropRoomThreePrev} ванная комната`;
-	}
-	else if (valueDropRoomThreePrev <= 4) {
-		bathroomsDropPrev = `, ${valueDropRoomThreePrev} ванные комнаты`;
-	}
-	else if (valueDropRoomThreePrev > 4) {
-		bathroomsDropPrev = `, ${valueDropRoomThreePrev} ванных комнат`;
-	};
+			let bathroomsDropPrev;
+			if (valueDropRoomThreePrev == 0) {
+				bathroomsDropPrev = ``;
+			} else if (valueDropRoomThreePrev == 1) {
+				bathroomsDropPrev = `, ${valueDropRoomThreePrev} ванная комната`;
+			}
+			else if (valueDropRoomThreePrev <= 4) {
+				bathroomsDropPrev = `, ${valueDropRoomThreePrev} ванные комнаты`;
+			}
+			else if (valueDropRoomThreePrev > 4) {
+				bathroomsDropPrev = `, ${valueDropRoomThreePrev} ванных комнат`;
+			};
 
-	let valueDropRoomSummPrev = valueDropRoomOnePrev + valueDropRoomTwoPrev + valueDropRoomThreePrev;
-	let inputDropRoomSummPrev = bedroomsPrev + bedDropPrev + bathroomsDropPrev;
-	if (valueDropRoomSummPrev == 0) {
-		inputDropRoom.value = inputDropRoomVar;
-	} else if (valueDropRoomSummPrev == 1) {
-		inputDropRoom.value = `${inputDropRoomSummPrev}`;
-	} else if (valueDropRoomSummPrev <= 4) {
-		inputDropRoom.value = `${inputDropRoomSummPrev}`;
-	} else if (valueDropRoomSummPrev > 4) {
-		inputDropRoom.value = `${inputDropRoomSummPrev}`;
-	};
+			let valueDropRoomSummPrev = valueDropRoomOnePrev + valueDropRoomTwoPrev + valueDropRoomThreePrev;
+			let inputDropRoomSummPrev = bedroomsPrev + bedDropPrev + bathroomsDropPrev;
+			if (valueDropRoomSummPrev == 0) {
+				inputDropRoom.value = inputDropRoomVar;
+			} else if (valueDropRoomSummPrev == 1) {
+				inputDropRoom.value = `${inputDropRoomSummPrev}`;
+			} else if (valueDropRoomSummPrev <= 4) {
+				inputDropRoom.value = `${inputDropRoomSummPrev}`;
+			} else if (valueDropRoomSummPrev > 4) {
+				inputDropRoom.value = `${inputDropRoomSummPrev}`;
+			};
 
-	if (valueDropRoomOnePrev >= 1) {
-		btnDropRoomMinus[0].style.color = "rgba(31, 32, 65, 0.5)";
-		btnDropRoomMinus[0].style.borderColor = "rgba(31, 32, 65, 0.5)";
-	}
-	if (valueDropRoomTwoPrev >= 1) {
-		btnDropRoomMinus[1].style.color = "rgba(31, 32, 65, 0.5)";
-		btnDropRoomMinus[1].style.borderColor = "rgba(31, 32, 65, 0.5)";
-	}
-	if (valueDropRoomThreePrev >= 1) {
-		btnDropRoomMinus[2].style.color = "rgba(31, 32, 65, 0.5)";
-		btnDropRoomMinus[2].style.borderColor = "rgba(31, 32, 65, 0.5)";
-	}
-
-	// Основной код подсчета гостей
-	dropRoom.addEventListener("click", function (event) {
-		let targetRoomDrop = event.target;
-		let dropRoomSpan;
-		if (targetRoomDrop.dataset.direction == "plus") {
-			dropRoomSpan = +targetRoomDrop.previousElementSibling.textContent;
-			targetRoomDrop.previousElementSibling.textContent = ++dropRoomSpan;
-			if (dropRoomSpan >= 1) {
-				targetRoomDrop.parentNode.firstElementChild.style.color = "rgba(31, 32, 65, 0.5)";
-				targetRoomDrop.parentNode.firstElementChild.style.borderColor = "rgba(31, 32, 65, 0.5)";
+			if (valueDropRoomOnePrev >= 1) {
+				btnDropRoomMinus[0].style.color = "rgba(31, 32, 65, 0.5)";
+				btnDropRoomMinus[0].style.borderColor = "rgba(31, 32, 65, 0.5)";
+			}
+			if (valueDropRoomTwoPrev >= 1) {
+				btnDropRoomMinus[1].style.color = "rgba(31, 32, 65, 0.5)";
+				btnDropRoomMinus[1].style.borderColor = "rgba(31, 32, 65, 0.5)";
+			}
+			if (valueDropRoomThreePrev >= 1) {
+				btnDropRoomMinus[2].style.color = "rgba(31, 32, 65, 0.5)";
+				btnDropRoomMinus[2].style.borderColor = "rgba(31, 32, 65, 0.5)";
 			}
 
-		}
-		if (targetRoomDrop.dataset.direction == "minus") {
-			dropRoomSpan = +targetRoomDrop.nextElementSibling.textContent;
-			dropRoomSpan = dropRoomSpan > 0 ? --dropRoomSpan : 0;
-			if (dropRoomSpan == 0) {
-				targetRoomDrop.style.color = "rgba(31, 32, 65, 0.25)";
-				targetRoomDrop.style.borderColor = "rgba(31, 32, 65, 0.25)";
+			// Основной код подсчета гостей
+			let targetRoomDrop = event.target;
+			let dropRoomSpan;
+			if (targetRoomDrop.dataset.direction == "plus") {
+				dropRoomSpan = +targetRoomDrop.previousElementSibling.textContent;
+				targetRoomDrop.previousElementSibling.textContent = ++dropRoomSpan;
+				if (dropRoomSpan >= 1) {
+					targetRoomDrop.parentNode.firstElementChild.style.color = "rgba(31, 32, 65, 0.5)";
+					targetRoomDrop.parentNode.firstElementChild.style.borderColor = "rgba(31, 32, 65, 0.5)";
+				}
+
 			}
-			targetRoomDrop.nextElementSibling.textContent = dropRoomSpan;
-		}
-		let valueDropRoomOne = +valueDropRoom[0].textContent;
-		let valueDropRoomTwo = +valueDropRoom[1].textContent;
-		let valueDropRoomThree = +valueDropRoom[2].textContent;
-		let bedrooms;
-		if (valueDropRoomOne == 0) {
-			bedrooms = `0 спален`;
-		} else if (valueDropRoomOne == 1) {
-			bedrooms = `${valueDropRoomOne} спальня`;
-		}
-		else if (valueDropRoomOne <= 4) {
-			bedrooms = `${valueDropRoomOne} спальни`;
-		}
-		else if (valueDropRoomOne > 4) {
-			bedrooms = `${valueDropRoomOne} спален`;
-		};
+			if (targetRoomDrop.dataset.direction == "minus") {
+				dropRoomSpan = +targetRoomDrop.nextElementSibling.textContent;
+				dropRoomSpan = dropRoomSpan > 0 ? --dropRoomSpan : 0;
+				if (dropRoomSpan == 0) {
+					targetRoomDrop.style.color = "rgba(31, 32, 65, 0.25)";
+					targetRoomDrop.style.borderColor = "rgba(31, 32, 65, 0.25)";
+				}
+				targetRoomDrop.nextElementSibling.textContent = dropRoomSpan;
+			}
+			let valueDropRoomOne = +valueDropRoom[0].textContent;
+			let valueDropRoomTwo = +valueDropRoom[1].textContent;
+			let valueDropRoomThree = +valueDropRoom[2].textContent;
+			let bedrooms;
+			if (valueDropRoomOne == 0) {
+				bedrooms = `0 спален`;
+			} else if (valueDropRoomOne == 1) {
+				bedrooms = `${valueDropRoomOne} спальня`;
+			}
+			else if (valueDropRoomOne <= 4) {
+				bedrooms = `${valueDropRoomOne} спальни`;
+			}
+			else if (valueDropRoomOne > 4) {
+				bedrooms = `${valueDropRoomOne} спален`;
+			};
 
-		let bedDrop;
-		if (valueDropRoomTwo == 0) {
-			bedDrop = ``;
-		} else if (valueDropRoomTwo == 1) {
-			bedDrop = `, ${valueDropRoomTwo} кровать`;
-		}
-		else if (valueDropRoomTwo <= 4) {
-			bedDrop = `, ${valueDropRoomTwo} кровати`;
-		}
-		else if (valueDropRoomTwo > 4) {
-			bedDrop = `, ${valueDropRoomTwo} кроватей`;
-		};
+			let bedDrop;
+			if (valueDropRoomTwo == 0) {
+				bedDrop = ``;
+			} else if (valueDropRoomTwo == 1) {
+				bedDrop = `, ${valueDropRoomTwo} кровать`;
+			}
+			else if (valueDropRoomTwo <= 4) {
+				bedDrop = `, ${valueDropRoomTwo} кровати`;
+			}
+			else if (valueDropRoomTwo > 4) {
+				bedDrop = `, ${valueDropRoomTwo} кроватей`;
+			};
 
-		let bathroomsDrop;
-		if (valueDropRoomThree == 0) {
-			bathroomsDrop = ``;
-		} else if (valueDropRoomThree == 1) {
-			bathroomsDrop = `, ${valueDropRoomThree} ванная комната`;
-		}
-		else if (valueDropRoomThree <= 4) {
-			bathroomsDrop = `, ${valueDropRoomThree} ванные комнаты`;
-		}
-		else if (valueDropRoomThree > 4) {
-			bathroomsDrop = `, ${valueDropRoomThree} ванных комнат`;
-		};
+			let bathroomsDrop;
+			if (valueDropRoomThree == 0) {
+				bathroomsDrop = ``;
+			} else if (valueDropRoomThree == 1) {
+				bathroomsDrop = `, ${valueDropRoomThree} ванная комната`;
+			}
+			else if (valueDropRoomThree <= 4) {
+				bathroomsDrop = `, ${valueDropRoomThree} ванные комнаты`;
+			}
+			else if (valueDropRoomThree > 4) {
+				bathroomsDrop = `, ${valueDropRoomThree} ванных комнат`;
+			};
 
-		let valueDropRoomSumm = valueDropRoomOne + valueDropRoomTwo + valueDropRoomThree;
-		let inputDropRoomSumm = bedrooms + bedDrop + bathroomsDrop;
-		if (valueDropRoomSumm == 0) {
-			inputDropRoom.value = inputDropRoomVar;
-		} else if (valueDropRoomSumm == 1) {
-			inputDropRoom.value = `${inputDropRoomSumm}`;
-		} else if (valueDropRoomSumm <= 4) {
-			inputDropRoom.value = `${inputDropRoomSumm}`;
-		} else if (valueDropRoomSumm > 4) {
-			inputDropRoom.value = `${inputDropRoomSumm}`;
-		};
-		event.preventDefault();
-	})
+			let valueDropRoomSumm = valueDropRoomOne + valueDropRoomTwo + valueDropRoomThree;
+			let inputDropRoomSumm = bedrooms + bedDrop + bathroomsDrop;
+			if (valueDropRoomSumm == 0) {
+				inputDropRoom.value = inputDropRoomVar;
+			} else if (valueDropRoomSumm == 1) {
+				inputDropRoom.value = `${inputDropRoomSumm}`;
+			} else if (valueDropRoomSumm <= 4) {
+				inputDropRoom.value = `${inputDropRoomSumm}`;
+			} else if (valueDropRoomSumm > 4) {
+				inputDropRoom.value = `${inputDropRoomSumm}`;
+			};
 
-	// --- выпадающий список комнат
-	const listDropRoom = document.querySelector('.dropdown-room');
+			// --- выпадающий список комнат
+			document.addEventListener("click", showList);
 
-	document.addEventListener("click", list);
-
-	function list(event) {
-		if (event.target.closest('.dropdown-room__input')) {
-			listDropRoom.classList.toggle('_active-dropdown-room');
-		};
-		if (!event.target.closest('.dropdown-room')) {
-			listDropRoom.classList.remove('_active-dropdown-room');
-		};
-	};
+			function showList(event) {
+				if (event.target.closest('.dropdown-room__input')) {
+					event.target.parentNode.classList.toggle('_active-dropdown-room');
+				};
+				if (!event.target.closest('.dropdown-room')) {
+					dropRoomElem.classList.remove('_active-dropdown-room');
+				};
+			};
+			event.preventDefault();
+		});
+	});
 };
