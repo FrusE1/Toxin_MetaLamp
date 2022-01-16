fad3431e72bc64fda3e708259cd07bdbdb23c9db
Marsel <fathutdinov.2000@mail.ru>
2022-01-16 18:12:35 +0300

    added paginator functional

--- src/components/buttons/paginator/paginator.js
+++ src/components/buttons/paginator/paginator.js
@@ -1,58 +1,118 @@
-// $('#pagination-con').pagination({
-//   dataSource: [1, 2, 3, 4, 5, 6, 7],
-//   callback: function (data, pagination) {
-//     // template method of yourself
-//     var html = template(data);
-//     $('#pagination').html(html);
-//   }
-// })
-// const ulTag = document.querySelector('.paginator__list');
+const ulTag = document.querySelector(".paginator__list");
+let totalPages = 15;
+function pagination(totalPages, page) {
+  let liTag = '';
+  let activeLi;
+  let beforePages = page - 1;
+  let afterPages = page + 1;
+  if (page > 1) {
+    liTag += `<li class="paginator__li-prev"><span class="paginator__span"><i class="paginator__arrow paginator__arrow-prev icon-arrow"></i></span></li>`;
+  };
 
-// let totalPages = 15;
-// function pagination(totalPages, page) {
-// 	let liTag = '';
-// 	let activeLi;
-// 	let beforePages = page - 1;
-// 	let afterPages = page + 1;
-// 	if (page > 1) {
-// 		liTag += `<li class="paginator__li-prev" onclick="pagination(totalPages, ${page - 1})"><span class="paginator__span"><i class="paginator__arrow paginator__arrow-prev icon-arrow"></i></span></li>`;
-// 	};
+  if (page > 2) {
+    liTag += `<li class="paginator__number"><span>1</span></li>`;
+    if (page > 3) {
+      liTag += `<li class="paginator__dots"><span>...</span></li>`;
+    };
+  };
 
-// 	if (page > 2) {
-// 		liTag += `<li class="paginator__number" onclick="pagination(totalPages, 1)"><span>1</span></li>`;
-// 		if (page > 3) {
-// 			liTag += `<li class="paginator__dots"><span>...</span></li>`;
-// 		};
-// 	};
+  let afterPagesList = afterPages + 1;
+  let pageLenght;
+  if (page <= 3) {
+    for (pageLenght = beforePages; pageLenght <= afterPagesList; pageLenght++) {
+      if (pageLenght > totalPages) {
+        continue;
+      };
 
-// 	for (let pageLenght = beforePages; pageLenght <= afterPages; pageLenght++) {
-// 		if (pageLenght > totalPages) {
-// 			continue;
-// 		};
+      if (pageLenght == 0) {
+        pageLenght = pageLenght + 1;
+      };
 
-// 		if (pageLenght == 0) {
-// 			pageLenght = pageLenght + 1;
-// 		};
+      if (page == pageLenght) {
+        activeLi = "paginator__number_active";
+      } else {
+        activeLi = "";
+      };
 
-// 		if (page == pageLenght) {
-// 			activeLi = "paginator__number_active";
-// 		} else {
-// 			activeLi = "";
-// 		};
+      liTag += `<li class="paginator__number ${activeLi}"><span>${pageLenght}</span></li>`;
+    };
+  };
+  if (page > 3) {
+    for (pageLenght = beforePages; pageLenght <= afterPages; pageLenght++) {
+      if (pageLenght > totalPages) {
+        continue;
+      };
 
-// 		liTag += `<li class="paginator__number ${activeLi}" onclick="pagination(totalPages, ${pageLenght})"><span>${pageLenght}</span></li>`;
-// 	};
+      if (pageLenght == 0) {
+        pageLenght = pageLenght + 1;
+      };
 
-// 	if (page < totalPages - 1) {
-// 		if (page < totalPages - 2) {
-// 			liTag += `<li class="paginator__dots"><span>...</span></li>`;
-// 		};
-// 		liTag += `<li class="paginator__number" onclick="pagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
-// 	};
+      if (page == pageLenght) {
+        activeLi = "paginator__number_active";
+      } else {
+        activeLi = "";
+      };
 
-// 	if (page < totalPages) {
-// 		liTag += `<li class="paginator__li-next" onclick="pagination(totalPages, ${page + 1})"><span class="paginator__span"><i class="paginator__arrow paginator__arrow-next icon-arrow"></i></span></li>`
-// 	};
-// 	ulTag.innerHTML = liTag;
-// };
-// pagination(totalPages, 1);
+      liTag += `<li class="paginator__number ${activeLi}"><span>${pageLenght}</span></li>`;
+    };
+  };
+
+  if (page < totalPages - 1) {
+    if (page < totalPages - 2) {
+      liTag += `<li class="paginator__dots"><span>...</span></li>`;
+    }
+    liTag += `<li class="paginator__number"><span>${totalPages}</span></li>`;
+  };
+
+  if (page < totalPages) {
+    liTag += `<li class="paginator__li-next"><span class="paginator__span"><i class="paginator__arrow paginator__arrow-next icon-arrow"></i></span></li>`
+  };
+  ulTag.innerHTML = liTag;
+
+  const nextPagination = document.querySelector(".paginator__li-next");
+  const prevPagination = document.querySelector(".paginator__li-prev");
+  const activePagination = document.querySelector('.paginator__number_active');
+  const nextActive = activePagination.nextElementSibling;
+  const prevtActive = activePagination.previousElementSibling;
+  if (page < 4) {
+    if (nextActive) {
+      const nextDouble = nextActive.nextElementSibling;
+      nextDouble.addEventListener('click', function () {
+        pagination(totalPages, page + 2);
+      });
+    };
+  };
+  if (nextActive) {
+    nextActive.addEventListener("click", function () {
+      pagination(totalPages, page + 1);
+    });
+  };
+  if (prevtActive) {
+    prevtActive.addEventListener("click", function () {
+      pagination(totalPages, page - 1);
+    });
+  };
+  if (page > 1) {
+    prevPagination.addEventListener('click', function () {
+      pagination(totalPages, page - 1);
+    });
+  };
+  if (page < totalPages) {
+    nextPagination.addEventListener('click', function () {
+      pagination(totalPages, page + 1);
+    });
+  };
+  if (prevPagination) {
+    const firstPages = prevPagination.nextElementSibling;
+    firstPages.addEventListener('click', function () {
+      pagination(totalPages, 1);
+    });
+  };
+  if (nextPagination) {
+    const lastPages = nextPagination.previousElementSibling;
+    lastPages.addEventListener('click', function () {
+      pagination(totalPages, totalPages);
+    });
+  };
+};
+pagination(totalPages, 1);
