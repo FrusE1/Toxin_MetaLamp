e33280a99dc1f779b68bc0ddd7cc84ea6203f10c
Marsel <fathutdinov.2000@mail.ru>
2022-02-24 20:09:11 +0300

    minor edits chart.js

--- src/pages/room-details/chart.js
+++ src/pages/room-details/chart.js
@@ -1,4 +1,5 @@
 import Chart from 'chart.js/auto';
+import { removeData } from 'jquery';
 const ctx = document.getElementById('myChart');
 const myChart = new Chart(ctx, {
   type: 'doughnut',
@@ -8,13 +9,14 @@ const myChart = new Chart(ctx, {
       label: '# of Votes',
       data: [130, 65, 65, 0],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)'
+        'rgba(255, 99, 132, 0.9)',
+        'rgba(54, 162, 235, 0.9)',
+        'rgba(255, 206, 86, 0.9)',
+        'rgba(75, 192, 192, 0.9)'
       ],
       spacing: 2,
-    }]
+      cutout: '89%',
+    }],
   },
   options: {
     plugins: {
@@ -28,6 +30,13 @@ const myChart = new Chart(ctx, {
           }
         }
       }
+    },
+    layout: {
+      padding: {
+        right: 10,
+      }
     }
-  }
+  },
 });
+ctx.canvas.parentNode.style.height = '120px';
+ctx.canvas.parentNode.style.width = '120px';
