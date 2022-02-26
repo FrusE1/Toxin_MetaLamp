58896c7c24efbe1a59131f6a1d01614df83a0b1a
Marsel <fathutdinov.2000@mail.ru>
2022-02-26 12:42:46 +0300

    completed chart.js

--- src/pages/room-details/chart.js
+++ src/pages/room-details/chart.js
@@ -1,22 +1,22 @@
 import Chart from 'chart.js/auto';
 import { removeData } from 'jquery';
 const ctx = document.getElementById('chart').getContext('2d');
-ctx.canvas.parentNode.style.height = "120px";
-ctx.canvas.parentNode.style.width = "120px";
-let chartArr = [130, 65, 65, 0]
+ctx.canvas.parentNode.style.height = "124px";
+ctx.canvas.parentNode.style.width = "124px";
+let chartArr = [130, 65, 65, 0];
 const myChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ['Великолепно', 'Хорошо', 'Удовлетворительно', 'Разочарован'],
+    labels: ['Великолепно', 'Удовлетворительно', 'Хорошо', 'Разочарован'],
     datasets: [{
       data: [chartArr[0], chartArr[1], chartArr[2], chartArr[3]],
       backgroundColor: [
         'rgba(255, 227, 156, 1)',
-        'rgba(111, 207, 151, 1)',
         'rgba(188, 156, 255, 1)',
+        'rgba(111, 207, 151, 1)',
         'rgba(145, 145, 145, 1)'
       ],
-      spacing: 2,
+      spacing: 0.5,
       cutout: '89%',
     }],
   },
@@ -24,14 +24,9 @@ const myChart = new Chart(ctx, {
     plugins: {
       legend: {
         display: false,
-        position: 'right',
-        align: 'end',
-        labels: {
-          font: {
-            family: "Montserrat",
-            size: 14,
-          }
-        },
+      },
+      tooltip: {
+        enabled: false,
       }
     },
     rotation: 180,
@@ -39,5 +34,5 @@ const myChart = new Chart(ctx, {
   },
 });
 // Центральный текст внутри диаграммы
-const chartText = document.querySelector('.chart__text span');
+const chartText = document.querySelector('.text-chart__num');
 chartText.innerHTML = `${chartArr[0] + chartArr[1] + chartArr[2] + chartArr[3]}`
